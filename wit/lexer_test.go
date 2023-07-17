6b2b972de7c424188b6997f22274be61cbbe333c
patrickhuber <patrickhuber@users.noreply.github.com>
2023-07-17 14:20:30 -0400

    simplifies lexer unit tests

--- wit/lexer_test.go
+++ wit/lexer_test.go
@@ -9,32 +9,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestLexer(t *testing.T) {
-	t.Run("line_comment", func(t *testing.T) {
-		str := "// this is a comment line"
-		lex := wit.NewLexer(strings.NewReader(str))
-		tok, err := lex.Next()
-		require.Nil(t, err)
-		require.NotNil(t, tok)
-		require.Equal(t, str, tok.Capture)
-		require.Equal(t, token.LineComment, tok.Type)
-	})
-	t.Run("block_comment", func(t *testing.T) {
-		str := "/* this is a comment block */"
-		lex := wit.NewLexer(strings.NewReader(str))
-		tok, err := lex.Next()
-		require.Nil(t, err)
-		require.NotNil(t, tok)
-		require.Equal(t, str, tok.Capture)
-		require.Equal(t, token.BlockComment, tok.Type)
-	})
-	t.Run("whitespace", func(t *testing.T) {
-		str := "\f\t "
-		lex := wit.NewLexer(strings.NewReader(str))
-		tok, err := lex.Next()
-		require.Nil(t, err)
-		require.NotNil(t, tok)
-		require.Equal(t, str, tok.Capture)
-		require.Equal(t, token.Whitespace, tok.Type)
-	})
+func TestLexerUnit(t *testing.T) {
+	type test struct {
+		name      string
+		str       string
+		tokenType token.TokenType
+	}
+	tests := []test{
+		{"line_comment", "// this is a comment line", token.LineComment},
+		{"block_comment", "/* this is a comment block */", token.BlockComment},
+		{"whitespace", "\f\t ", token.Whitespace},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			lex := wit.NewLexer(strings.NewReader(test.str))
+			tok, err := lex.Next()
+			require.Nil(t, err)
+			require.NotNil(t, tok)
+			require.Equal(t, test.str, tok.Capture)
+			require.Equal(t, test.tokenType, tok.Type)
+		})
+	}
 }
