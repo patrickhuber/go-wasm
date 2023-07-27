4a2db0f1ee8bc447598d8dab543a26fc0896781d
patrickhuber <patrickhuber@users.noreply.github.com>
2023-07-26 22:15:58 -0400

    adds parse-fail tests

--- wit/parse/parser_test.go
+++ wit/parse/parser_test.go
@@ -49,5 +49,41 @@ func TestParser(t *testing.T) {
 			require.NotNil(t, node)
 		})
 	}
+}
 
+func TestParseFail(t *testing.T) {
+	dir := "../../submodules/github.com/bytecodealliance/wasm-tools/crates/wit-parser/tests/ui/parse-fail"
+	files, err := os.ReadDir(dir)
+	require.NoError(t, err)
+	require.NotNil(t, files)
+
+	witFilter, err := regexp.Compile(".*[.]wit$")
+	require.NoError(t, err)
+
+	for _, file := range files {
+
+		if !witFilter.MatchString(file.Name()) {
+			continue
+		}
+		t.Run(file.Name(), func(t *testing.T) {
+			full := path.Join(dir, file.Name())
+
+			file, err := os.OpenFile(full, os.O_RDONLY, 0666)
+			require.NoError(t, err)
+			reader := bufio.NewReader(file)
+
+			var runes []rune
+			for {
+				r, _, err := reader.ReadRune()
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				require.NoError(t, err)
+				runes = append(runes, r)
+			}
+
+			_, err = wit.Parse(runes)
+			require.Error(t, err)
+		})
+	}
 }
