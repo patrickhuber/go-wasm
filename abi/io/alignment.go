9d75bfa69465fffa69642924fda1baf1a1d2c527
patrickhuber <patrickhuber@users.noreply.github.com>
2023-07-28 20:15:29 -0400

    switches to math.MaxInt

--- abi/io/alignment.go
+++ abi/io/alignment.go
@@ -113,7 +113,7 @@ func MaxCaseAlignment(cases []types.Case) (uint32, error) {
 func DiscriminantType(cases []types.Case) (types.ValType, error) {
 	n := len(cases)
 	// n must be positive integer
-	if !(0 < n && n < (1<<32)) {
+	if !(0 < n && n < math.MaxInt) {
 		return nil, fmt.Errorf("case length %d exceeds max %d", n, (1 << 32))
 	}
 	switch uint64(math.Ceil(math.Log2(float64(n)) / 8)) {
