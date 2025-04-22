635808fe2035ce97e2a832ee64ea61fd6d193d49
Patrick Huber <182398+patrickhuber@users.noreply.github.com>
2025-04-22 14:17:22 -0400

    adds failing test for i32 add

--- runtime/machine_test.go
+++ runtime/machine_test.go
@@ -1,6 +1,7 @@
 package runtime_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/patrickhuber/go-wasm/api"
@@ -9,14 +10,50 @@ import (
 )
 
 func TestMachine(t *testing.T) {
-	machine := runtime.NewMachine()
-	module := &api.Module{}
-	externals := []values.Value{}
-	returns, err := machine.Execute(module, externals)
-	if err != nil {
-		t.Error(err)
+	type test struct {
+		name            string
+		module          *api.Module
+		externals       []values.Value
+		expectedErr     error
+		expectedReturns []values.Value
 	}
-	if len(returns) != 0 {
-		t.Errorf("expected 0 returns but found %d", len(returns))
+	tests := []test{
+		{
+			name:            "empty",
+			module:          &api.Module{},
+			externals:       nil,
+			expectedErr:     nil,
+			expectedReturns: nil,
+		},
+		{
+			name:            "i32_add",
+			module:          &api.Module{},
+			externals:       []values.Value{values.I32Const(1), values.I32Const(2)},
+			expectedErr:     nil,
+			expectedReturns: []values.Value{values.I32Const(3)},
+		},
 	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			machine := runtime.NewMachine()
+			returns, err := machine.Execute(test.module, test.externals)
+			if test.expectedErr != nil && err == nil {
+				t.Errorf("expected error but found nil")
+			}
+			if test.expectedErr == nil && err != nil {
+				t.Error(err)
+			}
+			if len(returns) != len(test.expectedReturns) {
+				t.Errorf("expected %d returns but found %d", len(test.expectedReturns), len(returns))
+			}
+			for i, r := range returns {
+				expectedReturn := test.expectedReturns[i]
+				if reflect.DeepEqual(r, expectedReturn) {
+					continue
+				}
+				t.Error("expected returns to be equal")
+			}
+		})
+	}
+
 }
